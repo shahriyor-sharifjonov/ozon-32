305ef420f019a3902f954fc298b6411d68cd6ab6
Shahriyor <shahriyordeveloping@gmail.com>
2021-07-27 18:55:14 +0300

    initial

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,41 @@
-$( function() {
-  var dateFormat = "",
-    from = $( "#from" )
-      .datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        numberOfMonths: 1,
-        dateFormat: 'с dd.mm.yy',
-        minDate: -20, 
-        maxDate: "+1M +10D"
-      })
-      .on( "change", function() {
-        to.datepicker( "option", "minDate", getDate( this ) );
-      }),
-    to = $( "#to" ).datepicker({
-      defaultDate: "+1w",
-      changeMonth: true,
-      numberOfMonths: 1,
-      minDate: -20, 
-      dateFormat: 'по dd.mm.yy',
-      maxDate: "+1M +10D"
-    })
-    .on( "change", function() {
-      from.datepicker( "option", "maxDate", getDate( this ) );
-    });
 
-  function getDate( element ) {
-    var date;
-    try {
-      date = $.datepicker.parseDate( dateFormat, element.value );
-    } catch( error ) {
-      date = null;
-    }
+// $( function() {
+//   var dateFormat = "",
+//     from = $( "#from" )
+//       .datepicker({
+//         defaultDate: "+1w",
+//         changeMonth: true,
+//         numberOfMonths: 1,
+//         dateFormat: 'с dd.mm.yy',
+//         minDate: -20, 
+//         maxDate: "+1M +10D"
+//       })
+//       .on( "change", function() {
+//         to.datepicker( "option", "minDate", getDate( this ) );
+//       }),
+//     to = $( "#to" ).datepicker({
+//       defaultDate: "+1w",
+//       changeMonth: true,
+//       numberOfMonths: 1,
+//       minDate: -20, 
+//       dateFormat: 'по dd.mm.yy',
+//       maxDate: "+1M +10D"
+//     })
+//     .on( "change", function() {
+//       from.datepicker( "option", "maxDate", getDate( this ) );
+//     });
 
-    return date;
-  }
-} );
+//   function getDate( element ) {
+//     var date;
+//     try {
+//       date = $.datepicker.parseDate( dateFormat, element.value );
+//     } catch( error ) {
+//       date = null;
+//     }
+
+//     return date;
+//   }
+// } );
 
 
 var options = {
@@ -101,7 +102,3 @@ $( function() {
   });
 } );
 
-const bir = document.querySelectorAll('.ui-state-default');
-for(let i = 0; i < bir.length; i++){
-  
-}
